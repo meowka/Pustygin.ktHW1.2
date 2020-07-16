b43aa2029aa3d399f11c12e697257be18c2d2571
meowka <Pustygin.a@mail.ru>
2020-07-17 00:08:01 +0700

    refactor 0.2

--- src/main/kotlin/Analysis/main.kt
+++ src/main/kotlin/Analysis/main.kt
@@ -5,22 +5,38 @@ fun main() {
 }
 
 class Vk(
-    var id: Int,
-    var ownerId: Int,
-    var fromId: Int,
-    var createdBy: Int,
-    var date: Int,
-    var text: String,
-    var replyOwnerId: Int,
-    var replyPostId: Int,
-    var friendsOnly: Boolean,
+    id: Int,
+    ownerId: Int,
+    fromId: Int,
+    createdBy: Int,
+    date: Int,
+    text: String,
+    replyOwnerId: Int,
+    replyPostId: Int,
+    friendsOnly: Boolean,
     comments: Comments,
     likes: Likes,
-    views: Views
-
+    views: Views,
+    postType: PostType,
+    postSource: Any,
+    geo: Geo,
+    signerId: Int,
+    canPin: Boolean,
+    canDelete: Boolean,
+    canEdit: Boolean,
+    isPinned: Boolean,
+    markedAsAds: Boolean,
+    isFavorite: Boolean,
+    postponedId: Int
 ) {
 
 }
-class Likes(count:Int, userLikes:Boolean, canLikes:Boolean, canPublish:Boolean){}
-class Comments(count:Int, canPost:Boolean, groupsCanPost:Boolean, canClose:Boolean, canOpen:Boolean){}
-class Views(count:Int){}
+
+class Likes(count: Int, userLikes: Boolean, canLikes: Boolean, canPublish: Boolean) {}
+class Comments(count: Int, canPost: Boolean, groupsCanPost: Boolean, canClose: Boolean, canOpen: Boolean) {}
+class Views(count: Int) {}
+enum class PostType {
+    POST, COPY, REPLY, POSTPONE, SUGGEST
+}
+
+class Geo(type: String, coordinates: String, place: Any) {}
